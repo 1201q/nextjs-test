b689519d98cca389fc518f2f0fd6936a751da9e8
황준서 <1201q@naver.com>
2023-05-04 16:12:26 +0900

    라우터 없앴음

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,35 +4,32 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import axios from "axios";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 export default function Home({ data }) {
-  const router = useRouter();
-  const handleClick = (id) => {
-    router.push(
-      {
-        pathname: "/detail",
-        query: { id },
-      },
-      `/detail/${id}`
-    );
-  };
+  // const router = useRouter();
+  // const handleClick = (id) => {
+  //   router.push(
+  //     {
+  //       pathname: "/detail",
+  //       query: { id },
+  //     },
+  //     `/detail/${id}`
+  //   );
+  // };
 
   return (
     <>
       {data.map((item) => (
-        <div key={item.id} onClick={() => handleClick(item.id)}>
-          {item.title}
-        </div>
+        <div key={item.id}>{item.title}</div>
       ))}
     </>
   );
 }
 
-export const getServerSideProps = async ({ query }) => {
+export const getServerSideProps = async () => {
   try {
     const data = await axios
-      .get(`https://jsonplaceholder.typicode.com/posts?id=${query.id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts`)
       .then((res) => res.data);
     return { props: { data } };
   } catch (error) {
